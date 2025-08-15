ca0732965efee8d4fd5902e711cb15cfbfb740e0
Dharmit Parmar <182512111+Dharmit-Parmar@users.noreply.github.com>
2025-08-15 21:19:52 +0530

    solve some dsa questions
    Do some changes and attach notes

--- findPairsWithSum/findPairsWithSum.cpp
+++ findPairsWithSum/findPairsWithSum.cpp
@@ -20,7 +20,7 @@ void brootForceSum(int arr[], int size, int n)
             if (n == (arr[i] + arr[j]))
             {
                 cout << "Your number of pairs are : " << " ";
-                cout << arr[i] << " " << arr[j] << endl;
+                cout << arr[i] << " " << arr[j] << endl; 
             }
         }
     }
@@ -60,62 +60,7 @@ void afterSotingTheArr(int arr[], int size, int n)
     }
 }
 
-void finalApproch(int arr[], int size, int n)
-{
-    int i = 0, j = 0;
-    // for (int i = 0; i < size; i++)
-    // {
-    //     for (int j = i + 1; j < size; j++)
-    //     {
 
-    //         if (arr[i] > arr[j])
-    //         {
-    //             int temp;
-    //             temp = arr[i];
-    //             arr[i] = arr[j];
-    //             arr[j] = temp;
-    //         }
-    //     }
-    // }
-    while (i != size && j != size)
-    {
-        if (arr[i] > arr[j])
-        {
-            swap(arr[i], arr[j]);
-           
-        }
-         if(j ==size -1)
-            {
-                i++;
-                j = i ;
-                continue;
-            }
-            j++;
-    }
-
-    i = size - 1, j = 0;
-    while (i > j)
-    {
-
-        if ((arr[i] + arr[j]) > n)
-        {
-            i--;
-        }
-        else if ((arr[i] + arr[j]) < n)
-        {
-            j++;
-        }
-        else if ((arr[i] + arr[j]) == n)
-        {
-
-            cout << "The pairs are : " << arr[i] << " " << arr[j] << endl;
-            cout << "The pairs are : " << arr[j] << " " << arr[i] << endl;
-            i--;
-            j++;
-        }
-    }
-     
-}
 
 int main()
 {
