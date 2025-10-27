b40063835481ae8d46b739be2e4448450cf4b7a4
Dharmit Parmar <dharmit.parmar07@gmail.com>
2025-10-27 14:31:32 +0530

    Add initial Book/Member structs and ID generator functions
    Initializes the core data structures for the BiblioTech library manager.
    
    Defines the Book struct (title, author, isbn, etc.) and the Member struct (name, memberId, etc.).
    
    Creates the main Library class that will hold std::vectors of books and members.
    
    Implements genrateIsbn() to generate a 12-digit string by appending 9 random digits to "978" and then shuffling the newly added digits.
    
    Implements genrateMemberId() to generate a 10-digit random numeric string, followed by a full shuffle, and ensures the first digit is non-zero.
    
    Adds a main() function to test the genrateMemberId() output.

--- DSA Questions/Easy/FindTheMinMax/FindTheMinMax.cpp
+++ DSA Questions/Easy/FindTheMinMax/FindTheMinMax.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main() {
     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int size = sizeof(arr)/sizeof(arr[0]);
+    
     int min  , max = 0;
 
     for(int i = 0 ; i <10 ; i++)
