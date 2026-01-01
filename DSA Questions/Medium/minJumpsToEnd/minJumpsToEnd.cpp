f7cb2bfc7ca742154e3425d03c2b9d7bb75abee1
Dharmit Parmar <dharmit.parmar07@gmail.com>
2026-01-01 21:58:01 +0530

    reversing the stack

--- DSA Questions/Medium/minJumpsToEnd/minJumpsToEnd.cpp
+++ DSA Questions/Medium/minJumpsToEnd/minJumpsToEnd.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include <string>
-#include<vector>
+#include <vector>
 using namespace std;
 
-
-int minjump(vector<int>a)
+int minjump(vector<int> a)
 {
-    int minjump = 0 ;
-    int jumps = 0 ; 
-    if(a[0] < 1) { return -1;}
-    int i = 0 ;
-    while(i<a.size())
+    int minjump = 0;
+    int jumps = 0;
+    if (a[0] < 1)
+    {
+        return -1;
+    }
+    int i = 0;
+    while (i < a.size())
     {
         int n = a[i];
-        
     }
-
-
 }
 
-int main() {
-      vector<int> arr = {2,3,1,1,4};
+int main()
+{
+    vector<int> arr = {2, 3, 1, 1, 4};
     return 0;
 }
