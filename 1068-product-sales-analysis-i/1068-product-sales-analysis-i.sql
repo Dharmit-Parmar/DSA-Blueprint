719596be3cf8fd856e2707e335722e2cf830cae6
Dharmit Parmar <dharmit.parmar07@gmail.com>
2026-06-27 12:03:40 +0530

    Time: 1243 ms (64.21%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select p.product_name , s.year , s.price
-from Product as p
-inner join Sales as s
-on p.product_id = s.product_id
+# Write your MySQL query statement below
+select p.product_name,s.year,s.price 
+from Sales as s 
+left join Product as P 
+on s.product_id = p.product_id;
