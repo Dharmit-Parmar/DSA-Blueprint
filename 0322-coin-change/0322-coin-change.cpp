46bc80bab772f4a9f98a22b5b74b4051a7a87021
Dharmit Parmar <dharmit.parmar07@gmail.com>
2026-09-20 13:40:42 +0530

    Time: 18 ms (82.55%), Space: 17.8 MB (93.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // now doing with the dp
+        int inf = 1e9;
         int n = coins.size();
-        const int inf = 1e9;
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, inf));
-
-        for (int i = 0; i <= n; ++i) {
-            dp[i][0] = 0;
-        }
-
-        // take the coin first
-        for (int i = 1; i <= n; i++) {
-            int c = coins[i - 1];
-
-            // now we loop through the target
-            for (int j = 1; j <= amount; j++) {
-                // we exclude
-                int e = dp[i - 1][j];
-
-                // then we include
-
-                int in = inf;
-                if (j >= c) {
-                    if (dp[i][j - c] != inf) {
-                        in = 1 + dp[i][j - c];
-                    }
-                }
-
-                dp[i][j] = min(e, in);
+        vector<int> dp(amount + 1, inf);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] = min(dp[j], 1 + dp[j - coins[i]]);
             }
         }
-        return (dp[n][amount] == inf) ? -1 : dp[n][amount];
+        return (dp[amount] >= inf) ? -1 : dp[amount];
     }
 };
